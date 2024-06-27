3e7b5a3dfa148b6cd54900050eed538b2cf39ee9
RobinQu <robinqu@gmail.com>
2024-06-27 11:17:32 +0800

    chores: include instinct-cpp using conan

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+from conan.tools.build import check_min_cppstd
+from conan.tools.cmake import cmake_layout
+
+
+class CompressorRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps"
+
+    def validate(self):
+        check_min_cppstd(self, 20)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("instinct-cpp/0.1.5")
